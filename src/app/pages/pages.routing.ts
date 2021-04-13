7f788080625572d70f02fc8362d75c3cfd070a21
¨Christian <christianespinoza935@gmail.com>
2021-04-13 10:19:17 -0500

    Fin seccion 4 - Router

---
+++ src/app/pages/pages.routing.ts
@@ -0,0 +1,29 @@
+/** Va a tener  la definicion de las rutas que estan internas al directorio page*/
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+import { PagesComponent } from './pages.component';
+
+const routes: Routes = [
+  // Rutas hijas
+  {
+    //Ruta a partir de un path especifico
+    path: 'dashboard',
+    component: PagesComponent,
+    children: [
+      { path: '', component: DashboardComponent },
+      { path: 'progress', component: ProgressComponent },
+      { path: 'grafica1', component: Grafica1Component },
+      // { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class PagesRoutingModule {}
